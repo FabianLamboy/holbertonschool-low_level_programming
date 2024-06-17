6f8b7a11e21af7c5542d0a30f060ba4047600403
FabianLamboy <fabian.lamboyperez@gmail.com>
2024-06-17 13:45:19 -0400

    c is fun!

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Prints the size
+ * Description - Prints the size of various types
+ * Return: Ends the script
+ */
+int main(void)
+{
+	printf("Size of char: %zu byte(s)\n", sizeof(char));
+	return (0);
+}
