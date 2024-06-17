cdfa2124d3738887cf852120b82bf9bc061caf90
FabianLamboy <fabian.lamboyperez@gmail.com>
2024-06-17 13:50:58 -0400

    c is fun!

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 	printf("Size of char: %zu byte(s)\n", sizeof(char));
-	printf("Size of shot: %zu byte(s)\n", sizeof(short));
+	printf("Size of short: %zu byte(s)\n", sizeof(short));
 	printf("Size of int: %zu byte(s)\n", sizeof(int));
 	printf("Size of long: %zu byte(s)\n", sizeof(long));
 	return (0);
