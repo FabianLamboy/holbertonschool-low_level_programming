09bf0315f3d804a3b3619097d2cdaddef1ce5760
FabianLamboy <fabian.lamboyperez@gmail.com>
2024-06-17 12:25:39 -0400

    c is fun!

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-int main(void)
-{
-	int puts("Programming is like building a multilingual puzzle ");
-	return (0);
+
+int main() {
+    // Using puts to print the desired string
+    puts("Programming is like building a multilingual puzzle,");
+
+    // End the program with the value 0
+    return 0;
 }
+
