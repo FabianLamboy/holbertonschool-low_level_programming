5c742a39b6904e893e6e73ab5b425ae1cc9a1913
FabianLamboy <fabian.lamboyperez@gmail.com>
2024-06-17 13:12:55 -0400

    c is fun!

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Prints a text
+ * Description - Prints a text to stdout
+ * Return: Ends the command
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
