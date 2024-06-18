403cf0a61cca3432c3dc36898e26b3c86d9be49a
FabianLamboy <fabian.lamboyperez@gmail.com>
2024-06-18 17:57:57 -0400

    c is fun!

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,8 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /**
- *  main - aaaaaaaaa
- *  Description - aaaaaaaaa
+ *  main - Random number in a variable
+ *  Description - The variable will store a random
+ *  number every time program runs
  *  Return: Ends the code
  */
 int main(void)
@@ -11,8 +12,15 @@ int main(void)
 	int n;
 
 	srand(time(0));
-
 	n = rand() - RAND_MAX / 2;
-	/* my code here */
+
+	printf("Last digit of %d is %d", n, n % 10);
+
+	if (n % 10 > 5)
+		printf(" and is greater than 5\n");
+	else if (n % 10 == 0)
+		printf(" and is 0\n");
+	else
+		printf(" and is less than 6 and not 0\n");
 	return (0);
 }
