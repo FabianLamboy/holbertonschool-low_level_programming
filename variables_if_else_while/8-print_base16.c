37b742d367f0688547d5fb7a1d1ee2c825864a5d
FabianLamboy <fabian.lamboyperez@gmail.com>
2024-06-18 14:06:48 -0400

    c is fun!

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 /**
-*main - aaaaa
-*Description - aaaaaa
+*main - Prints all numbers
+*Description - Prints all numbers of base sixteen in lowercase
 *Return: Ends the code
 */
 int main(void)
 {
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+		putchar(c);
+	for (c = 'a'; c <= 'f'; c++)
+		putchar(c);
+	putchar('\n');
 return (0);
 }
