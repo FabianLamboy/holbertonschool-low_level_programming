ef3b729b68db363a37a26c17190bf0dbdb67f4ad
FabianLamboy <fabian.lamboyperez@gmail.com>
2024-06-18 10:52:30 -0400

    c is fun!

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Print the number within the variable
+ * Description - The source code will print whether the number
+ * Return: Ends the code
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	return (0);
+}
