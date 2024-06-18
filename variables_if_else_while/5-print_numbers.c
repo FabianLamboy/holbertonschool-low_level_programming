060214d0e57c75277a6846b269dd0682c0ee7264
FabianLamboy <fabian.lamboyperez@gmail.com>
2024-06-18 13:33:03 -0400

    c is fun!

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 /**
- *  main - aaaaaaa
- *  Description - aaaaaaaa
+ *  main - Prints all numbers
+ *  Description - Prints all numbers from zero to ten
  *  Return: Ends the code
  */
 int main(void)
 {
+	int i
+		;
+	for (i = 0; i <= 9; ++i)
+		putchar('0' + i);
+	putchar('\n');
 	return (0);
 }
