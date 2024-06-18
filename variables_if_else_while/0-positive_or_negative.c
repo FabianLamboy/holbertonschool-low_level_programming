f04296a13f3f12c546881b7e8c3248d520b58e22
FabianLamboy <fabian.lamboyperez@gmail.com>
2024-06-18 11:24:10 -0400

    c is fun!

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
  * main - Print the number within the variable
  * Description - The source code will print whether the number
@@ -10,6 +11,20 @@ int main(void)
 	int n;
 
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
+	printf("The number %d, \n");
+	if (n > 0)
+	{
+		printf("is positive\n");
+	}
+	else if (n < 0)
+	{
+		printf("is negative\n");
+	}
+	else
+	{
+		printf("is zero\n");
+	}
 	return (0);
 }
