597c7b844553f33ad1ae794ff4a1266c10aeb624
FabianLamboy <fabian.lamboyperez@gmail.com>
2024-06-18 11:37:46 -0400

    c is fun!

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /**
  * main - Print the number within the variable
- * Description - The source code will print whether the number
+ * Description - The source code will print the number
  * Return: Ends the code
  */
 int main(void)
@@ -11,20 +11,18 @@ int main(void)
 	int n;
 
 	srand(time(0));
-
 	n = rand() - RAND_MAX / 2;
-	printf("The number %d, \n");
 	if (n > 0)
 	{
-		printf("is positive\n");
+		printf("The variable is positive\n");
 	}
-	else if (n < 0)
+	if (n < 0)
 	{
-		printf("is negative\n");
+		printf("The variable is negative\n");
 	}
-	else
+	if (n == 0)
 	{
-		printf("is zero\n");
+		printf("The variable is zero\n");
 	}
 	return (0);
 }
