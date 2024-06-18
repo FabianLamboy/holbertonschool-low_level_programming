49f9cc8d0f861c8e82801c76dbe78ee8785339c9
FabianLamboy <fabian.lamboyperez@gmail.com>
2024-06-18 14:33:54 -0400

    c is fun!

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,11 +8,11 @@ int main(void)
 {
 	int c;
 
-	for (c = 0; c < 10; c++)
-		putchar(c + '0');
-	if (c < 9)
-		putchar(',');
-	putchar(' ');
+	for (c = 48; c < 58; c++)
+		putchar(c);
+	if (c < 57)
+		putchar(44);
+	putchar(32);
 	putchar('\n');
 	return (0);
 }
