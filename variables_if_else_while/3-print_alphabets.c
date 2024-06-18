9dd129e9df47d5e6fb4c769df494fa9d2b0330ea
FabianLamboy <fabian.lamboyperez@gmail.com>
2024-06-18 13:16:45 -0400

    c is fun!

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 /**
- *  main - aaaaaaaaa
- *  Description - aaaaaaaaa
+ *  main - Prints the alphabet
+ *  Description - Prints the alphabet in both uppercase and lowercase
  *  Return: Ends the code
  */
 int main(void)
 {
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; ++c)
+		putchar(c);
+	putchar('\n');
 	return (0);
 }
