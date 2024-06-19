ee1e6a3d041cbf1d09521300747e141c37a2cace
FabianLamboy <fabian.lamboyperez@gmail.com>
2024-06-19 10:23:29 -0400

    c is fun!

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -5,32 +5,16 @@
  * by a new line
  * Return: Ends the command
  */
-int _putchar(char c);
-/**
- * Declaration of _putchar
- * Description - _putchar
- * Return: alwayd 0
- */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 	return (0);
-}
-/**
- * _putchar - prints the characters
- * Description: help me
- * Return: always 0
- * */
-int _putchar(char c)
-{
-	return putchar(c);
 }
