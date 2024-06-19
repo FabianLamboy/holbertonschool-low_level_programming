f509334c714a44e8b7c7d1d0c7abeb0eadec8e0c
FabianLamboy <fabian.lamboyperez@gmail.com>
2024-06-19 12:29:23 -0400

    c is fun!

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - Enters the code
+ * Description - Prints the alhpabet
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
