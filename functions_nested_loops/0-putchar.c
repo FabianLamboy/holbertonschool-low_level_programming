171e48d29c092cb26e029f3c678586cc78f4ca71
FabianLamboy <fabian.lamboyperez@gmail.com>
2024-06-19 09:49:29 -0400

    c is fun!

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Prints
+ * Description - Prints _putchar, followed
+ * by a new line
+ * Return: Ends the command
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
