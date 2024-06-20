23d7eca5895d74350954bbd599b8f6406ff9db92
FabianLamboy <fabian.lamboyperez@gmail.com>
2024-06-20 13:16:41 -0400

    c is fun!

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - prints a square, followed by a new line
+ * Return: always success
+ * @size: th number of squares/number of times
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
