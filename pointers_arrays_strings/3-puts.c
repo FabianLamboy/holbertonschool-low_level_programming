361df0acf1c3e8ba2cfb3ff1a5fb3c9b7528c072
FabianLamboy <fabian.lamboyperez@gmail.com>
2024-06-25 14:45:47 -0400

    c is fun!

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,15 +1,17 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _puts - prints a string
  * @str: the string to print
  * Return: void
  */
+
 void _puts(char *str)
+
 {
 	while (*str != '\0')
 	{
 		_putchar(*str);
-		str++;
 	}
 	_putchar('\n');
 }
